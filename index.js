3f329c1664d4c18c1d6061bcc4f827d7ea53ee17
PAINxNAGATO01~ <harshrawat349@gmail.com>
2022-06-08 06:57:52 +0530

    modified

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const requests = require('requests');
 const homeFile = fs.readFileSync('./home.html','utf-8')
 
 const replaceVal = (tempVal,orgVal)=>{
-   let temp = tempVal.replace("{%tempVal%}",orgVal.main.temp)
-   temp = temp.replace("{%tempMin%}",orgVal.main.temp_min)
-   temp = temp.replace("{%tempMax%}",orgVal.main.temp_max)
+   let temp = tempVal.replace("{%tempVal%}",(orgVal.main.temp-273).toFixed(2))
+   temp = temp.replace("{%tempMin%}",(orgVal.main.temp_min-273).toFixed(2))
+   temp = temp.replace("{%tempMax%}",(orgVal.main.temp_max-273).toFixed(2))
    temp = temp.replace("{%city%}",orgVal.name)
    temp = temp.replace("{%country%}",orgVal.sys.country)
 
@@ -19,7 +19,7 @@ const replaceVal = (tempVal,orgVal)=>{
 
 app.get('/',(req,res) =>{
   
-  requests('https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=d4521a823f2019276eb54439da75dbaf')
+  requests('https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=d4521a823f2019276eb54439da75dbaf')
   .on('data',(chunk) =>{
     const objData = JSON.parse(chunk);
     const arr = [objData];
